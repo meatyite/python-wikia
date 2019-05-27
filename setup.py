e16f16a18d5a1af71f97d67bbe68a3a10d5f2ff5
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2019-05-27 19:41:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='python-wikia',
       version='1.0.0',
       description='Object oriented wikia (fandom.com) API v1 wrapper',
-      long_description='A Wrapper for fandom.com\'s API.',
+      long_description='An object oriented Wrapper for fandom.com\'s API.',
       author='sl4v',
       author_email='iamsl4v@protonmail.com',
       url='https://github.com/sl4vkek/python-wikia',
